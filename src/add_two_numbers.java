7496d8eb3fb8e43a974e5751d69186e88613bf86
waleedmalik7 <waleedmalik027@gmail.com>
2022-07-28 04:55:29 -0400

    giving time exceeded
    issue is with the carry variable

---
+++ src/add_two_numbers.java
@@ -0,0 +1,28 @@
+class add_two_numbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode header =  new ListNode(0);
+        ListNode l3 = header;
+
+        int carry = 0;
+
+        while(l1!= null || l2 != null || carry != 0){
+
+            int first = l1 == null ? 0 :l1.val;
+            int second = l2 == null ? 0 :l2.val;
+            int sum  = first + second + carry;    
+           
+            l3.next = new ListNode(sum  % 10);
+            if (sum > 9) { 
+                carry = sum / 10; 
+            }
+
+            l1 = l1 == null ? null: l1.next;
+            l2 = l2 == null ? null: l2.next;
+
+            l3 = l3.next;
+        }
+        return header.next;
+        
+    }
+}
